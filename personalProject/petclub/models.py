f998e48ca58970b6dfa84426e4a498debc80ba8b
YasminUwu <yasmin.talamilla@alumnos.ucn.cl>
2022-05-03 23:02:24 -0400

    build: Preparando el main

---
+++ personalProject/petclub/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+'''class B(model.Model):
+    pass
+'''
+class Pet(models.Model):
+    id = models.AutoField(primary_key=True) 
+    species = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    color = models.CharField(max_length=50)
+    weight = models.IntegerField()
+
+  #  object = PetManager()
+    
+    class Meta:
+        managed = True
+
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    rut = models.CharField(max_length=12)
+    age = models.IntegerField(null=True, blank=True)
+    person = models.ManyToManyField
+
+    class Meta:
+        managed = True
---
+++ personalProject/petclub/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+# Create your views here.
+# modulos de DRF
+from rest_framework.response import Response
+from rest_framework.views import APIView
+'''from rest_framework.generics import(
+    RetrieveAPIView
+    ListAPIView
+)
+class HelloWorld(APIView):
+    def get(self, request): # verbo de la peticion como un metodo
+        # logica asociada al endpoint
+        return Response(data="Hola gente, estoy en el get", status=200) # respuesta del servicio
+    def get(self, request): 
+        return Response(data="Hola gente, estoy en el patch", status=200) 
+    def get(self, request):
+        return Response(data="Hola gente, estoy en el delete", status=200) 
+    def get(self, request):
+        return Response(data="Hola gente, estoy en el post", status=200) '''
+class PersonView(APIView):
+    def get(self, request):
+        return Response(data="Hola gente, estoy en el get", status=200) 
+    def post(self, request):
+        return Response(data = "Hola gente, estoy en el post", status = 200)
+    def patch(self, request):
+        return Response(data="Hola gente, estoy en el patch", status = 200)
+    def delete(self,request):
+        return Response(data = "Hola gente, estoy en el delete", status = 200)
+class PetView(APIView):
+    def get(self, request):
+        return Response(data="Hola gente, estoy en el get", status=200) 
+    def post(self, request):
+        return Response(data = "Hola gente, estoy en el post", status = 200)
+    def patch(self, request):
+        return Response(data="Hola gente, estoy en el patch", status = 200)
+    def delete(self,request):
+        return Response(data = "Hola gente, estoy en el delete", status = 200)
+
+
+'''class PetListAPIView(ListAPIView):
+    def get(self, request):
+        return Response(data="Hola a todos, estas son mis mascotas")
+
+class PetAPIView(RetrieveAPIView):
+    def get(self, request):
+
+class PetListAPIView(RetrieveAPIView):
+
+    
+class HelloWorldOnlyGet(RetrieveAPIView):
+    pass'''
